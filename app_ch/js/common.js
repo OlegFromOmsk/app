f7abf3a43fb4665547720ba6e28185efc771ed2e
OlegFromOmsk <32164733+OlegFromOmsk@users.noreply.github.com>
2018-04-11 15:11:56 +0700

    Add files via upload

--- app_ch/js/common.js
+++ app_ch/js/common.js
@@ -1,3 +1,31 @@
 $(function() {
+	
+	$('#about_car').owlCarousel({
+		loop: true,
+		items: 1,
+		margin: 0,
+		dots: false,
+		nav: true,
+		autoplay: false,
+		smartSpeed: 700
+	});
+
+	$('#review_car').owlCarousel({
+		loop: true,
+		margin: 0,
+		dots: false,
+		nav: true,
+		autoplay: false,
+		smartSpeed: 700,
+		responsive: {
+			0: {
+				items: 1
+			},
+			950: {
+				items: 2
+			}
+		},
+		navText: ["&lsaquo;", "&rsaquo;	"]
+	});
 
 });
